5e94860d282c8d25d281f017e508011905690495
xdeng9 <xdeng9@gmail.com>
2020-06-05 21:49:26 -0700

    Add maze grid and style

--- src/graph/Maze.js
+++ src/graph/Maze.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import './maze.css';
 
 class Maze extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.rows = 35;
+        this.cols = 60;
+
+        this.handleMaze = this.handleMaze.bind(this);
+        this.handlePath = this.handlePath.bind(this);
+    }
 
     componentDidMount() {
         document.getElementById('maze').classList.add('active');
@@ -8,10 +18,42 @@ class Maze extends React.Component {
         document.getElementById("astar").classList.remove("active");
     }
 
+    handleMaze(e) {
+        e.preventDefault();
+        document.getElementById('10-10').classList.add('hollow');
+    }
+
+    handlePath(e) {
+        e.preventDefault();
+    }
+
+    createGrid() {
+        let grid = [];
+        for (let i = 0; i < this.rows; i++) {
+            grid.push([]);
+            for (let j = 0; j < this.cols; j++) {
+                grid[i][j] = (
+                    <div
+                        id={`${i}-${j}`}
+                        key={`${i}-${j}`}
+                        className="solid"
+                    ></div>
+                );
+            }
+        }
+        return grid;
+    }
+
     render() {
         return (
-            <div>
-                Hello from Maze!
+            <div className="maze-container">
+                <div className="maze-controll">
+                    <button onClick={this.handleMaze}>Create Maze</button>
+                    <button onClick={this.handlePath} className="show-path-btn">Show Path</button>
+                </div>
+                <div className="maze-grid">
+                    {this.createGrid()}
+                </div>
             </div>
         )
     }
