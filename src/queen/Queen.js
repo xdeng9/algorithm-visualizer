b80b6fdfbfd32a1a7168cb85e57847ce65989f77
xdeng9 <xdeng9@gmail.com>
2020-06-14 21:38:57 -0700

    Add component for n queen

---
+++ src/queen/Queen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Queen extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                n queens...
+            </div>
+        )
+    }
+}
+
+export default Queen;
