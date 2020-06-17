81167a46034b5e18660c1dee25d02a774fd8b934
xdeng9 <xdeng9@gmail.com>
2020-06-16 23:30:07 -0700

    Add createChessBoard

--- src/queen/Queen.js
+++ src/queen/Queen.js
@@ -2,11 +2,34 @@ import React from 'react';
 
 class Queen extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.row = 8;
+        this.col = 8;
+    }
+
     componentDidMount() {
         document.getElementById('queen').classList.add('active');
         document.getElementById('maze').classList.remove('active');
         document.getElementById('dijkstra').classList.remove('active');
         document.getElementById("astar").classList.remove("active");
+    }
+
+    createChessBoard() {
+        let board = [];
+        for (let i = 0; i < this.row; i++) {
+            let row = [];
+            for (let j = 0; j < this.col; j++) {
+                let cellNum = i * this.row + j;
+                row.push(
+                    <div>
+
+                    </div>
+                )
+            }
+            board.push(row);
+        }
+        return board;
     }
     
     render() {
